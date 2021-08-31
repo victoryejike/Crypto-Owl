1556b12833ac057354a64225b3b15fd24f25113f
Ejike Victory <victoryejike@gmail.com>
2021-08-31 19:55:03 +0100

    added the web3 code to the NewDesign page and added the metamask button

--- src/views/NewDesign.vue
+++ src/views/NewDesign.vue
@@ -27,16 +27,178 @@
         >
       </div>
     </div>
+    <button
+      class="btn btn-wallet"
+      :class="activeAddress ? 'btn-disable' : null"
+      @click="handleConnect"
+    >
+      <img
+        src="@img/metamask.png"
+        alt="metamask logo"
+        class="metamask-logo"
+      >
+      <span
+        v-if="activeAddress"
+        class="btn-text"
+      >
+        `${activeAddress}`
+      </span>
+      <span
+        class="btn-text"
+      >
+        Connect<br>Metamask
+      </span>
+    </button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+// import { defineComponent } from 'vue';
+import { IReceipt, ITokenMetadata } from '@type/interface';
+import axios from '@util/axios';
+import {
+  computed,
+  defineComponent, onMounted, reactive, ref,
+} from 'vue';
+import Web3 from 'web3';
+import { useStore } from 'vuex';
+import Metamask from '@/utils/metamask';
+import toast from '@/utils/toast';
+import { ToastType } from '@/types/enums';
+import 'vue-skeletor/dist/vue-skeletor.css';
+
+const ContractABI = require('@/assets/abi/owls-uat.json');
+
+const CONTRACT_ADDRESS = process.env.VUE_APP_CONTRACT_ADDRESS;
+const pricePerNFT = 0.03;
 
 export default defineComponent({
   name: 'ViewTemplate',
   setup(props, { attrs, slots, emit }) {
+    let closeMsg: () => void | null;
+    const totalBuy = ref(1);
+    const isLoading = ref(true);
+    const contract = Metamask.initContract(ContractABI, CONTRACT_ADDRESS);
+    const metadataList = ref<ITokenMetadata[]>([]);
+    const input = reactive({
+      amount: 1,
+      toAddress: null,
+      transferTokenID: null,
+      address: null,
+      tokenID: null,
+    });
+
+    const store = useStore();
+    const activeAddress = computed(() => store.getters['data/activeAddress']);
+
+    const isConnected = () => {
+      if (!activeAddress.value) {
+        toast({
+          title: 'Please connect address first',
+          type: ToastType.FAILED,
+        });
+        return false;
+      }
+      return true;
+    };
+
+    const walletOfOwner = async (address: string): Promise<string[]> => {
+      const result: string[] = await contract?.methods.walletOfOwner(address).call() || [];
+      return result;
+    };
+
+    const getMetaData = async (tokenID: string): Promise<ITokenMetadata> => {
+      const result = await contract?.methods.tokenURI(tokenID).call();
+      return (await axios.get(result))?.data;
+    };
+
+    const mint = async (amount: number) => {
+      if (!isConnected()) { return; }
+
+      await contract?.methods.mint(activeAddress.value, amount)
+        .send({ from: activeAddress.value, value: Web3.utils.toWei((amount * pricePerNFT).toString()) })
+        .once('transactionHash', (hash: string) => {
+          closeMsg = toast({
+            title: 'Waiting the transaction write into the blockchain...',
+            type: ToastType.LOADING,
+            timeout: -1,
+          });
+        })
+        .once('receipt', async (receipt: IReceipt) => {
+          closeMsg();
+          if (receipt.status) {
+            toast({
+              title: 'Transaction Succeed',
+              type: ToastType.SUCCESS,
+            });
+            const tokenList = await walletOfOwner(activeAddress.value);
+            const cacheList: Promise<ITokenMetadata>[] = [];
+            for (let index = tokenList.length - amount; index < tokenList.length; index += 1) {
+              cacheList.push(getMetaData(tokenList[index]));
+            }
+            try {
+              metadataList.value = await Promise.all(cacheList);
+            } catch (error) {
+              console.error(error);
+              toast({
+                title: 'Server Error',
+                type: ToastType.FAILED,
+              });
+            }
+          }
+        })
+        .on('error', (error: Error) => {
+          console.error(error);
+        });
+    };
+
+    const transferFrom = async (to: string, tokenID: string) => {
+      if (!isConnected()) { return; }
+
+      await contract?.methods.transferFrom(activeAddress.value, to, tokenID)
+        .send({ from: activeAddress.value })
+        .once('transactionHash', async (hash: string) => {
+          //
+        })
+        .once('receipt', async (receipt: IReceipt) => {
+          //
+        })
+        .on('error', (error: Error) => {
+          console.error(error);
+        });
+    };
+
+    const handleConnect = async () => {
+      const address = await Metamask.getActiveAddress();
+      if (address) {
+        store.commit('data/setActiveAddress', address);
+        store.commit('data/setIsAccountLocked', false);
+        Metamask.detectingAccount();
+      }
+    };
+
+    const setTotoalBuy = (total: number) => {
+      if (total > 0 && total < 21) { totalBuy.value = total; }
+    };
+
+    onMounted(async () => {
+      if (await Metamask.detectingChain()) {
+        Metamask.initAccount();
+        Metamask.detectingAccount();
+      }
+    });
+
     return {
+      mint,
+      transferFrom,
+      walletOfOwner,
+      handleConnect,
+      isLoading,
+      metadataList,
+      input,
+      activeAddress,
+      totalBuy,
+      setTotoalBuy,
     };
   },
 });
@@ -82,8 +244,30 @@ export default defineComponent({
   padding: 0.9em 2em;
 }
 
+.btn-wallet {
+  display: flex;
+  align-items: center;
+  font-weight: 600;
+  align-self: start;
+  margin: 0 4rem;
+  margin-top: 7.3rem;
+  background-color: rgba(0, 0, 0, 0.678);
+  color: #fff;
+  border: 1px solid #fff;
+  border-radius: 100px;
+}
+
+.btn-text {
+  text-align: left;
+}
+
 .btn:active {
   transform: translateY(0.1em);
+}
+
+.metamask-logo {
+  height: 2.5rem;
+  margin-right: 0.8rem;
 }
 
 .social-media-div {
